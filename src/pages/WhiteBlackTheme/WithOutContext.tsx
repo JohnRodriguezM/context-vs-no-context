82d552e72766f04979e2d67b76e9008a070b6e4d
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-10-17 22:09:29 -0500

    establishing initial structure

--- src/pages/WhiteBlackTheme/WithOutContext.tsx
+++ src/pages/WhiteBlackTheme/WithOutContext.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export const WithOutContext = () => {
   return (
-    <div>
-      dark and white theme
-      WithOutContext APIII</div>
-  )
-}
+    <div className=" text-base font-bold underline text-red-600">
+      <header>
+        <h2> dark and white theme with out context API</h2>
+        <h3>Mi cabecera</h3>
+      </header>
+      <main>
+        <h2>Mi main content</h2>
+      </main>
+      <footer>
+        <h3>Mi footer</h3>
+      </footer>
+    </div>
+  );
+};
